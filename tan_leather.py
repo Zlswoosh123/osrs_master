4ab7749aee923e03168b6a03fa2c86c4b066edf7
zlswoosh123 <zlswoosh123@gmail.com>
2023-11-06 13:04:23 -0800

    Script clicks on banker

--- tan_leather.py
+++ tan_leather.py
@@ -15,10 +15,6 @@ import core
 import yaml
 from PIL import Image
 from functions import Image_count
-from functions import image_Rec_clicker
-from functions import screen_Image
-from functions import release_drop_item
-from functions import drop_item
 from functions import Image_to_Text
 from functions import random_breaks
 from functions import invent_crop
@@ -34,6 +30,7 @@ from functions import offscreen_mouse
 from functions import super_random_breaks
 from functions import safe_open
 from functions import double_random
+from functions import findarea_attack_quick
 
 # Variables
 global hwnd
@@ -45,6 +42,7 @@ newTime_break = False
 global timer
 global timer_break
 global ibreak
+Run_Duration_hours = 5
 timer_log = 0
 runelite = functions.runelite
 inv_cap = random.randint(14, 17) # init inv cap to read in status
@@ -94,82 +92,30 @@ except BaseException:
     core.printWindows()
     print(BaseException)
     pass
-def random_break(start, c):
-    global newTime_break
-    startTime = time.time()
-    # 1200 = 20 minutes
-    a = random.randrange(0, 4)
-    # if startTime - start > c:
-    #     options[a]()
-    #     newTime_break = True
 
-
-def randomizer(timer_breaks, ibreaks):
-    global newTime_break
-    global timer_break
-    global ibreak
-    random_break(timer_breaks, ibreaks)
-    if newTime_break == True:
-        timer_break = timer()
-        ibreak = random.randrange(600, 2000)
-        newTime_break = False
-
-    # b = random.uniform(4, 5)
-
-
-def timer():
-    startTime = time.time()
-    return startTime
-
-
-def random_pause():
-    b = random.uniform(20, 250)
-    print('pausing for ' + str(b) + ' seconds')
-    time.sleep(b)
-    newTime_break = True
-
-def tan_leather():
-    print('Starting tan_leather which should click spell')
-    global actions
-    global runelite
-    actions = "Making eel sushi - always hot but never seared"
-    invent_crop()
-
-    double_random(1, 2)
-    # Drop click items
-        xham= random.randint(655,660) + random.randint(-7, 10) # 648, 670
-        yham= random.randint(505,515) + random.randint(-5,5) # 500, 520
-        hammer_coord =(xham, yham)
-        move_wait = double_random(.1, .22)
-        pyautogui.moveTo(hammer_coord, duration=move_wait)
-        click_wait = double_random(.1, .22)
-        print('Trying to click coord!')
-        screen_front(runelite)
-        pyautogui.click(hammer_coord, duration=click_wait, button='left')
-
-        image_eel_clicker(r'infernal_eel.png', 'Clicking Eel', 2, 2, 0.95, 'left', 10, False, True)
-
-
-def find_fish(showCoords=False, left=0, top=0, right=800, bottom=800, boundaries=[([110, 100, 0], [195, 180, 60])]):
+# Boundaries are color boundaries
+def find_banker(showCoords=False, left=0, top=0, right=800, bottom=800, boundaries=[([110, 100, 0], [195, 180, 60])]):
     global window
     functions.screen_Image(left, top, right, bottom)
     image = cv2.imread('images/screenshot.png')
     safe_open(image, 'screenshot.png')
-    image = cv2.rectangle(image, pt1=(600, 0), pt2=(850, 200), color=(0, 0, 0), thickness=-1)
-    image = cv2.rectangle(image, pt1=(0, 0), pt2=(150, 100), color=(0, 0, 0), thickness=-1)
+    # image = cv2.rectangle(image, pt1=(600, 0), pt2=(850, 200), color=(0, 0, 0), thickness=-1)
+    # image = cv2.rectangle(image, pt1=(0, 0), pt2=(150, 100), color=(0, 0, 0), thickness=-1)
 
     # loop over the boundaries
     for (lower, upper) in boundaries:
         # create NumPy arrays from the boundaries
-        lower = np.array(lower, dtype="uint8")
-        upper = np.array(upper, dtype="uint8")
+        lower, upper = np.array(lower, dtype="uint8"), np.array(upper, dtype="uint8")
         safe_open(image, 'screenshot.png')
-        # find the colors within the specified boundaries and apply the mask
-        print(f'image before mask is {image}')
+        # print(f'image before mask is {image}')
         contours = ""
+        # find the colors within the specified boundaries and apply the mask
+        # Pixels below 40 turn black, rest turn white (255). 0 is type of op, ignore
         if image is not None:
             mask = cv2.inRange(image, lower, upper)
             ret, thresh = cv2.threshold(mask, 40, 255, 0)
+            # External: specifies that only external contours are detected (those that form the boundary of an object).
+            # Approx_none: specifies that all contour points are stored, without any compression or approximation.
             contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     if len(contours) != 0:
         c = max(contours, key=cv2.contourArea)
@@ -185,17 +131,45 @@ def find_fish(showCoords=False, left=0, top=0, right=800, bottom=800, boundaries
         return (x, y)
     else:
         return False
-def pick_random_fishing_spot(top_ss=0, left_ss=0, right_ss=800, bottom_ss=800, showCoords=False):
-    if window == 2:
-        left_ss, top_ss, right_ss, bottom_ss = 0 + 875, 0, 1500, 800
-    elif window == 3:
-        left_ss, top_ss, right_ss, bottom_ss = 1920, 0, 800+1920, 800
-    elif window == 4:
-        left_ss, top_ss, right_ss, bottom_ss = 1920, 0, 1500+1920, 800
-    else:
-        left_ss, top_ss, right_ss, bottom_ss = 0, 0, 800, 800
-    fish = find_fish(False, left_ss, top_ss, right_ss, bottom_ss)
-    return fish
+def pick_banker(top_ss=0, left_ss=0, right_ss=800, bottom_ss=800, showCoords=False):
+    left_ss, top_ss, right_ss, bottom_ss = 0, 0, 800, 800
+    banker = find_banker(False, left_ss, top_ss, right_ss, bottom_ss)
+    return banker
+
+def tan_leather(monster='Banker', take_human_break=True, Run_Duration_hours=3):
+    print('Starting tan_leather which should click spell')
+    global actions
+    global runelite
+    actions = "Making eel sushi - always hot but never seared"
+    invent_crop()
+    double_random(1, 2) # safety wait, possibly remove
+    # Open Bank
+    coords = functions.findarea_attack_quick(3)
+    if coords[0] != 0:  # attack npc/monster
+        c = double_random(2,4)
+        print(f'coords[0] is: {coords[0]} and coords[1] is {coords[1]}')
+        time.sleep(c)
+        if take_human_break:
+            c = random.triangular(0.1, 50, 3)
+            time.sleep(c)
+    print('Ending powerattack_text')
+    # Withdraw Hides
+
+    # Close Bank
+
+    # Open Magic menu
+
+    # Move mouse and click spell x5 (check tick delay)
+
+    # Move to tan leather spell
+    # move_wait = double_random(.1, .22)
+    # pyautogui.moveTo(hammer_coord, duration=move_wait)
+    # click_wait = double_random(.1, .22)
+    # print('Trying to click coord!')
+    # screen_front(runelite)
+    # pyautogui.click(hammer_coord, duration=click_wait, button='left')
+    #
+    # image_eel_clicker(r'infernal_eel.png', 'Clicking Eel', 2, 2, 0.95, 'left', 10, False, True)
 
 def timer_countdown():
     global Run_Duration_hours
@@ -208,103 +182,74 @@ def timer_countdown():
     for i in range(final):
         # the exact output you're looking for:
         if timer_log % 10 == 0:
-            print(bcolors.OK + f'\r[%-10s] %d%%' % ('='*round((i/final)*10), round((i/final)*100)), f'time left: {(t_end - time.time())/60 :.2f} mins | coords: {coords} | status: {fishing_text} | fish: {fish_count} | clues: {clue_count} | {actions}| Drop at: {inv_cap}', end='')
-        timer_log += 1
-        time.sleep(1)
+            timer_log += 1
+            time.sleep(1)
         if timer_log % 10 == 0:
             functions.screen_Image(0, 0, 800, 800)
 
-def powerfisher(fish_type, Run_Duration_hours=6):
-    global ibreak, coords, fishing_text, time_left, powerlist, actions, t_end, fish_count, clue_count, invent_count
-    t_end = time.time() + (60 * 60 * Run_Duration_hours)
-
-    print('Will break in: %.2f' % (ibreak / 60) + ' minutes |', "Fish Type Selected:", fish_type)
-    t1 = Thread(target=timer_countdown)
-    t1.start()
-
-    while time.time() < t_end:
-        global inv_cap
-        randomizer(timer_break, ibreak)
-        resizeImage()
-        fishing_text = Image_to_Text('thresh', 'textshot.png')
-        print(f'Emptying inv when at {inv_cap} items')
-        approved_text = ['fishing', 'ishing', 'fishin']
-        if fishing_text.strip().strip("'").strip(';').lower() not in approved_text:
-            random_breaks(0.2, 3)
-            pick_random_fishing_spot(fish_type,)
-            if random.randint(1,6) == 3:
-                functions.offscreen_mouse()
-            time.sleep(super_random_breaks(8, 11, 15, 18))
-            resizeImage()
-        # if skill_lvl_up() != 0:
-        #     actions = 'level up'
-        #     random_breaks(0.2, 3)
-        #     pyautogui.press('space')
-        #     random_breaks(0.1, 3)
-        #     pyautogui.press('space')
-        #     a = random.randrange(0, 2)
-        #     # print(a)
-        #     spaces(a)
-        actions = 'none'
-        invent_crop()
-        resizeImage()
-        # main_good = functions.invent_count(fish_type + '.png', .95)
-        hammer = functions.invent_count('hammer.png', .75)
-        fish_count = functions.invent_count(fish_type + '.png', .95)
-        print(f'fish count is: {fish_count}')
-        print(f'hammer count is: {hammer}')
-        invent = fish_count
-        if invent == 0:
-            actions = 'opening inventory'
-            screen_front(runelite)
-            pyautogui.press('esc')
-            time.sleep(random.randint(3, 6))
-        if invent >= inv_cap:
-            random_breaks(0.2, 0.7)
-            drop_fish()
-            random_breaks(0.2, 0.7)
-            inv_cap = random.randint(16, 21)
-
-
-
-
-coords = (0, 0)
-actions = 'None'
-fishing_text = 'Not Fishing'
-time_left = 0
-
-#-------------------------------
-
-invent_count = 0
-
-#use the other names to fly/cage fish
-fish_type = functions.fish_type # 'trout_fish' # 'lobster_fish' 'salmon_fish', 'prawn_fish#
-fish_count = 0
-clue_count = 0
-#-------------------------------
 
 def rand_click(xrand, yrand):
     xrand = random.randrange(100, 450)
     yrand = random.randrange(250, 600)
     return xrand, yrand
+
+def test(monster='Banker', burybones=False, Pickup_loot=False, Take_Human_Break=False,
+                         Run_Duration_hours=6):
+    print('Starting powerattack_text!')
+    global coords, combat_text, time_left, powerlist, actions, powerlist, t_end
+    t1 = Thread(target=timer_countdown)
+    t1.start()
+    t_end = time.time() + (60 * 60 * Run_Duration_hours)
+    monster_list = ['Banker', 'chicken', 'guard', 'cow', 'monk', 'imp', 'skeleton', 'dwarf', 'giant frog', 'goblin']
+    monster_array = [['Banker'], ['chicken'], ['guard', 'gua rd'], ['cow', 'cou'], ['monk'], ['imp'], ['skeleton'], ['dwarf'],
+                     ['giant frog', 'giant', 'frog'], ['goblin']]
+    group = monster_list.index(monster)
+    while time.time() < t_end:
+        print('Start of while loop')
+        r = random.uniform(0.1, 5)
+        functions.resizeImage_combat()
+        # combat_text = functions.Image_to_Text_combat('thresh', 'screen_resize.png')
+        attack = 0
+        for monsters in monster_array[group]:
+            print(f'Current monster is: {monster}')
+            print(combat_text.strip().lower().find(monsters))
+            if combat_text.strip().lower().find(monsters) == -1:
+                attack += 1
+        if attack == len(monster_array[group]):
+            d = random.uniform(0.05, 0.1)
+            time.sleep(d)
+            coords = findarea_attack_quick(3)
+            off = random.randint(0, 5)
+            if off == 3:
+                offscreen_mouse()
+            if coords[0] != 0:  # attack npc/monster
+                c = random.uniform(2, 4)
+                print(f'coords[0] is: {coords[0]} and coords[1] is {coords[1]}')
+                time.sleep(c)
+                if Take_Human_Break:
+                    c = random.triangular(0.1, 50, 3)
+                    time.sleep(c)
+            print('Ending test')
+
 if __name__ == "__main__":
-    print(f'window is {window}')
-    xrand = 0
-    yrand = 0
-    xrand, yrand = rand_click(xrand, yrand)
-    print(f'xrand is {xrand}')
-    print(f'yrand is {yrand}')
-    time.sleep(1)
-    resizeImage()
-    invent_crop()
-    fish_count = functions.invent_count(fish_type + '.png', .95)
-    x = xrand
-    print(f'Right click x cord is: {x}')
-    y = yrand
-    print(f'Right click y cord is: {y}')
-    pyautogui.click(x, y, button='right')
-    ibreak = random.randrange(300, 2000)
-    timer_break = timer()
-    # --------- CHANGE TO RUN FOR AMOUNT OF HOURS ----------------
-    Run_Duration_hours = 5.1
-    powerfisher(fish_type, Run_Duration_hours=Run_Duration_hours)
+    test(monster='Banker', burybones=False, Pickup_loot=False, Take_Human_Break=False,
+         Run_Duration_hours=6)
+    # print(f'window is {window}')
+    # xrand = 0
+    # yrand = 0
+    # xrand, yrand = rand_click(xrand, yrand)
+    # print(f'xrand is {xrand}')
+    # print(f'yrand is {yrand}')
+    # time.sleep(1)
+    # resizeImage()
+    # invent_crop()
+    # x = xrand
+    # print(f'Right click x cord is: {x}')
+    # y = yrand
+    # print(f'Right click y cord is: {y}')
+    # pyautogui.click(x, y, button='right')
+    # ibreak = random.randrange(300, 2000)
+    # # timer_break = timer()
+    # # --------- CHANGE TO RUN FOR AMOUNT OF HOURS ----------------
+    # Run_Duration_hours = 5.1
+
